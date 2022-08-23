c74cd70ec5b229c3c1f662aae8731577099c9960
avocadojesus <fred@platejoy.com>
2022-08-23 08:28:38 -0700

    revive a few lines of code from js spike

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,50 @@
 import express, { Express, Request, Response } from 'express'
+import fs from 'fs'
+import { faker } from '@faker-js/faker'
+import generateResponse from './helpers/generateResponse'
 
 const app: Express = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server')
+if (!process.env.ENDPOINTS_PATH) {
+  throw 'cannot start test api server without first setting path to endpoints.json (use ENDPOINTS_PATH env to set this)'
+}
+
+console.log('extracting endpoints.json file...')
+const results = fs.readFileSync(process.env.ENDPOINTS_PATH)?.toString()
+
+if (!results) {
+  throw 'endpoints.json file was not found.'
+}
+
+let endpoints: {[key: string]: any}
+try {
+  endpoints = JSON.parse(results)
+} catch (error) {
+  throw(`Failed to parse JSON for endpoints.json file. Error: ${error}`)
+}
+
+Object.keys(endpoints).forEach(endpointKey => {
+  const [httpMethod, path] = endpointKey.split(':')
+
+  switch(httpMethod) {
+  case 'POST':
+    app.post(path, (_, res) => res.json(generateResponse(endpoints[endpointKey].payload_shape)))
+    break
+
+  case 'GET':
+    app.get(path, (_, res) => res.json(generateResponse(endpoints[endpointKey].payload_shape)))
+    break
+
+  default:
+    throw `Unrecognized HTTP method discovered in ${process.env.ENDPOINTS_PATH}: ${httpMethod}`
+  }
+  // app.get('/', (_, res) => {
+  //   res.send('hello world')
+  // })
 })
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
+app.listen(3000, () => {
+  console.log("listening on the port 3000...")
 })
+
