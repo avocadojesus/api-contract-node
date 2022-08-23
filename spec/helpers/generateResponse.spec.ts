f7f5015137b5bf5e294a5aa981fb95efe3665f53
avocadojesus <fred@platejoy.com>
2022-08-23 09:02:16 -0700

    cleanup

--- spec/helpers/generateResponse.spec.ts
+++ spec/helpers/generateResponse.spec.ts
@@ -1,20 +1,5 @@
-import generateResponse, {
-  UNIX_REGEX,
-  ANSIC_REGEX,
-  KITCHEN_REGEX,
-  RFC1123_REGEX,
-  RFC1123Z_REGEX,
-  RFC3339_REGEX,
-  RFC3339_NANO_REGEX,
-  RFC822_REGEX,
-  RFC822Z_REGEX,
-  RFC850_REGEX,
-  RUBY_DATE_REGEX,
-  STAMP_REGEX,
-  STAMP_MICRO_REGEX,
-  STAMP_MILLI_REGEX,
-  STAMP_NANO_REGEX,
-} from '../../src/helpers/generateResponse'
+import generateResponse from '../../src/helpers/generateResponse'
+import { DATETIME_FORMATS } from '../../src/config/formats'
 const ISO861_DATE_REGEX = /\d{4}-\d{2}-\d{2}/
 const YYMMDD_REGEX = /\d{2}-\d{2}-\d{2}/
 const MMDDYY_REGEX = /\d{2}-\d{2}-\d{2}/
@@ -111,92 +96,82 @@ describe ('generateResponse', () => {
   context ('datetime decorators', () => {
     it ('can parse a datetime:ansic response', () => {
       const res = generateResponse({ datetime: 'datetime:ansic' })
-      expect(res.datetime).toMatch(ANSIC_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.ansic.regex)
     })
 
     it ('can parse a datetime:kitchen response', () => {
       const res = generateResponse({ datetime: 'datetime:kitchen' })
-      expect(res.datetime).toMatch(KITCHEN_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.kitchen.regex)
     })
 
     it ('can parse a datetime:rfc1123 response', () => {
       const res = generateResponse({ datetime: 'datetime:rfc1123' })
-      expect(res.datetime).toMatch(RFC1123_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.rfc1123.regex)
     })
 
     it ('can parse a datetime:rfc1123z response', () => {
       const res = generateResponse({ datetime: 'datetime:rfc1123z' })
-      expect(res.datetime).toMatch(RFC1123Z_REGEX)
-    })
-
-    it ('can parse a datetime:rfc1123z response', () => {
-      const res = generateResponse({ datetime: 'datetime:rfc1123z' })
-      expect(res.datetime).toMatch(RFC1123Z_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.rfc1123z.regex)
     })
 
     it ('can parse a datetime:rfc3339 response', () => {
       const res = generateResponse({ datetime: 'datetime:rfc3339' })
-      expect(res.datetime).toMatch(RFC3339_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.rfc3339.regex)
     })
 
     it ('can parse a datetime:rfc3339_nano response', () => {
       const res = generateResponse({ datetime: 'datetime:rfc3339_nano' })
-      expect(res.datetime).toMatch(RFC3339_NANO_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.rfc3339_nano.regex)
     })
 
     it ('can parse a datetime:rfc822 response', () => {
       const res = generateResponse({ datetime: 'datetime:rfc822' })
-      expect(res.datetime).toMatch(RFC822_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.rfc822.regex)
     })
 
     it ('can parse a datetime:rfc822z response', () => {
       const res = generateResponse({ datetime: 'datetime:rfc822z' })
-      expect(res.datetime).toMatch(RFC822Z_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.rfc822z.regex)
     })
 
     it ('can parse a datetime:rfc850 response', () => {
       const res = generateResponse({ datetime: 'datetime:rfc850' })
-      expect(res.datetime).toMatch(RFC850_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.rfc850.regex)
     })
 
     it ('can parse a datetime:ruby_date response', () => {
       const res = generateResponse({ datetime: 'datetime:ruby_date' })
-      expect(res.datetime).toMatch(RUBY_DATE_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.ruby_date.regex)
     })
 
     it ('can parse a datetime:stamp response', () => {
       const res = generateResponse({ datetime: 'datetime:stamp' })
-      expect(res.datetime).toMatch(STAMP_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.stamp.regex)
     })
 
     it ('can parse a datetime:stamp_micro response', () => {
       const res = generateResponse({ datetime: 'datetime:stamp_micro' })
-      expect(res.datetime).toMatch(STAMP_MICRO_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.stamp_micro.regex)
     })
 
     it ('can parse a datetime:stamp_milli response', () => {
       const res = generateResponse({ datetime: 'datetime:stamp_milli' })
-      expect(res.datetime).toMatch(STAMP_MILLI_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.stamp_milli.regex)
     })
 
     it ('can parse a datetime:stamp_nano response', () => {
       const res = generateResponse({ datetime: 'datetime:stamp_nano' })
-      expect(res.datetime).toMatch(STAMP_NANO_REGEX)
-    })
-
-    it ('can parse a datetime:stamp_nano response', () => {
-      const res = generateResponse({ datetime: 'datetime:stamp_nano' })
-      expect(res.datetime).toMatch(STAMP_NANO_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.stamp_nano.regex)
     })
 
     it ('can parse a datetime:unix response', () => {
       const res = generateResponse({ datetime: 'datetime:unix' })
-      expect(res.datetime).toMatch(UNIX_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.unix.regex)
     })
 
     it ('can parse a datetime:unix_date response', () => {
       const res = generateResponse({ datetime: 'datetime:unix_date' })
-      expect(res.datetime).toMatch(UNIX_REGEX)
+      expect(res.datetime).toMatch(DATETIME_FORMATS.unix.regex)
     })
   })
 })
---
+++ src/config/formats.ts
@@ -0,0 +1,117 @@
+export const ANSIC_LUXON_FORMAT = 'ccc MMM dd HH:mm:ss y'
+export const KITCHEN_LUXON_FORMAT = 'hh:mma'
+export const RFC1123_LUXON_FORMAT = 'ccc, dd MMM y HH:mm:ss ZZZZ'
+export const RFC1123Z_LUXON_FORMAT = 'ccc, dd MMM y HH:mm:ss ZZZ'
+export const RFC3339_LUXON_FORMAT = "y-MM-dd'T'HH:mm:ssZZ"
+export const RFC3339_NANO_LUXON_FORMAT = "y-MM-dd'T'HH:mm:ss.SZZ"
+export const RFC822_LUXON_FORMAT = 'dd MMM yy HH:mm ZZZZ'
+export const RFC822Z_LUXON_FORMAT = 'dd MMM yy HH:mm ZZZ'
+export const RFC850_LUXON_FORMAT = 'cccc, dd-MMM-yy HH:mm:ss ZZZZ'
+export const RUBY_DATE_LUXON_FORMAT = 'ccc MMM dd HH:mm:ss ZZZ y'
+export const STAMP_LUXON_FORMAT = 'MMM dd HH:mm:ss'
+export const STAMP_MICRO_LUXON_FORMAT = 'MMM dd HH:mm:ss.S'
+export const STAMP_MILLI_LUXON_FORMAT = 'MMM dd HH:mm:ss.S'
+export const STAMP_NANO_LUXON_FORMAT = 'MMM dd HH:mm:ss.S'
+export const UNIX_LUXON_FORMAT = 'ccc MMM dd HH:mm:ss ZZZZ y'
+
+export const ANSIC_REGEX = /^[a-zA-Z]+ [a-zA-Z]+ \d{2} \d{2}:\d{2}:\d{2} \d{4}$/
+export const KITCHEN_REGEX = /\d{1,2}:\d{2}(AM|PM)/
+export const RFC1123_REGEX = /^[a-zA-Z]+, \d{1,2} [a-zA-Z]+ \d{4} \d{2}:\d{2}:\d{2} [A-Z]{3}/
+export const RFC1123Z_REGEX = /^[a-zA-Z]+, \d{1,2} [a-zA-Z]+ \d{4} \d{2}:\d{2}:\d{2} (\+|-)\d{4}/
+export const RFC3339_REGEX = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\+|-)\d{2}:\d{2}/
+export const RFC3339_NANO_REGEX = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{2,6}(\+|-)\d{2}:\d{2}/
+export const RFC822_REGEX = /^\d{1,2} [a-zA-Z]+ \d{2} \d{2}:\d{2} [A-Z]{3}/
+export const RFC822Z_REGEX = /^\d{1,2} [a-zA-Z]+ \d{2} \d{2}:\d{2} (\+|-)\d{4}/
+export const RFC850_REGEX = /^[A-Za-z]+, \d{2}-[A-Za-z]+-\d{2} \d{2}:\d{2}:\d{2} [A-Z]{3}/
+export const RUBY_DATE_REGEX = /^[A-Za-z]+ [A-Za-z]+ \d{2} \d{2}:\d{2}:\d{2} (\+|-)\d{4} \d{4}/
+export const STAMP_REGEX = /^[A-Za-z]{3} \d{2} \d{2}:\d{2}:\d{2}/
+export const STAMP_MICRO_REGEX = /^[A-Za-z]{3} \d{2} \d{2}:\d{2}:\d{2}\.\d+/
+export const STAMP_MILLI_REGEX = /^[A-Za-z]{3} \d{2} \d{2}:\d{2}:\d{2}\.\d+/
+export const STAMP_NANO_REGEX = /^[A-Za-z]{3} \d{2} \d{2}:\d{2}:\d{2}\.\d+/
+export const UNIX_REGEX = /^[a-zA-Z]+ [a-zA-Z]+ \d{2} \d{2}:\d{2}:\d{2} [A-Z]{3} \d{4}$/
+
+export enum AcceptedDatetimeFormats {
+  Ansic='ansic',
+  Kitchen='kitchen',
+  RFC1123='rfc1123',
+  RFC1123Z='rfc1123z',
+  RFC3339='rfc3339',
+  RFC3339Nano='rfc3339_nano',
+  RFC822='rfc822',
+  RFC822Z='rfc822z',
+  RFC850='rfc850',
+  RubyDate='ruby_date',
+  Stamp='stamp',
+  StampMicro='stamp_micro',
+  StampMilli='stamp_milli',
+  StampNano='stamp_nano',
+  Unix='unix',
+  UnixAlternate='unix_date',
+}
+
+export const DATETIME_FORMATS = {
+  [AcceptedDatetimeFormats.Ansic]: {
+    luxon: ANSIC_LUXON_FORMAT,
+    regex: ANSIC_REGEX,
+  },
+  [AcceptedDatetimeFormats.Kitchen]: {
+    luxon: KITCHEN_LUXON_FORMAT,
+    regex: KITCHEN_REGEX,
+  },
+  [AcceptedDatetimeFormats.RFC1123]: {
+    luxon: RFC1123_LUXON_FORMAT,
+    regex: RFC1123_REGEX,
+  },
+  [AcceptedDatetimeFormats.RFC1123Z]: {
+    luxon: RFC1123Z_LUXON_FORMAT,
+    regex: RFC1123Z_REGEX,
+  },
+  [AcceptedDatetimeFormats.RFC3339]: {
+    luxon: RFC3339_LUXON_FORMAT,
+    regex: RFC3339_REGEX,
+  },
+  [AcceptedDatetimeFormats.RFC3339Nano]: {
+    luxon: RFC3339_NANO_LUXON_FORMAT,
+    regex: RFC3339_NANO_REGEX,
+  },
+  [AcceptedDatetimeFormats.RFC822]: {
+    luxon: RFC822_LUXON_FORMAT,
+    regex: RFC822_REGEX,
+  },
+  [AcceptedDatetimeFormats.RFC822Z]: {
+    luxon: RFC822Z_LUXON_FORMAT,
+    regex: RFC822Z_REGEX,
+  },
+  [AcceptedDatetimeFormats.RFC850]: {
+    luxon: RFC850_LUXON_FORMAT,
+    regex: RFC850_REGEX,
+  },
+  [AcceptedDatetimeFormats.RubyDate]: {
+    luxon: RUBY_DATE_LUXON_FORMAT,
+    regex: RUBY_DATE_REGEX,
+  },
+  [AcceptedDatetimeFormats.Stamp]: {
+    luxon: STAMP_LUXON_FORMAT,
+    regex: STAMP_REGEX,
+  },
+  [AcceptedDatetimeFormats.StampMicro]: {
+    luxon: STAMP_MICRO_LUXON_FORMAT,
+    regex: STAMP_MICRO_REGEX,
+  },
+  [AcceptedDatetimeFormats.StampMilli]: {
+    luxon: STAMP_MILLI_LUXON_FORMAT,
+    regex: STAMP_MILLI_REGEX,
+  },
+  [AcceptedDatetimeFormats.StampNano]: {
+    luxon: STAMP_NANO_LUXON_FORMAT,
+    regex: STAMP_NANO_REGEX,
+  },
+  [AcceptedDatetimeFormats.Unix]: {
+    luxon: UNIX_LUXON_FORMAT,
+    regex: UNIX_REGEX,
+  },
+  [AcceptedDatetimeFormats.UnixAlternate]: {
+    luxon: UNIX_LUXON_FORMAT,
+    regex: UNIX_REGEX,
+  },
+}
