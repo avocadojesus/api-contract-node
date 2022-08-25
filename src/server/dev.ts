a36fd1b35d217d0d2229faa29fb4c82bab3de3e1
avocadojesus <fred@platejoy.com>
2022-08-25 10:33:08 -0700

    automatically assign dummy api-contract.json file for dev server

--- src/server/dev.ts
+++ src/server/dev.ts
@@ -1,7 +1,7 @@
 import createServer from './helpers/createServer'
 
 const PORT = process.env.API_CONTRACT_PORT || 4000
-const app = createServer()
+const app = createServer('spec/endpoint_stubs/nested/api-contract.json')
 
 app.listen(PORT, () => {
   console.log(`listening on the port ${PORT}...`)
