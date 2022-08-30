d1a465b1b291dafe4cababc2cb02e97e9841e1ae
avocadojesus <fred@platejoy.com>
2022-08-29 22:42:48 -0700

    remove comma separation

--- src/exceptions/missing-endpoint-coverage.ts
+++ src/exceptions/missing-endpoint-coverage.ts
@@ -11,7 +11,7 @@ export default class MissingEndpointCoverage extends Error {
       ATTENTION:
         The following endpoints have not been checked by your suite for compliance:
 
-        ${this.endpointKeys.join(",\n        ")}
+        ${this.endpointKeys.join("\n        ")}
 
       In order to obtain full coverage, make sure you run the .toPassCompliance on all
       endpoints within your api-contract.json file.
