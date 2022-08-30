e205da0a08c3b0ce1e9fbb2b5165cdc19baeaceb
avocadojesus <fred@platejoy.com>
2022-08-29 20:55:11 -0700

    remove bin

--- bin/mock-api.ts
+++
@@ -1,7 +0,0 @@
-import { exec } from 'child_process'
-
-exec('yarn --cwd ./node_modules/api-contract-node start', (err, stdout, stderr) => {
-  if (err) throw err
-  console.log(`stdout: ${stdout}`)
-  console.log(`stderr: ${stderr}`)
-})
