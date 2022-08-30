d3df5a3ef1d557f23f84dbea8321e403d8ffba9b
avocadojesus <fred@platejoy.com>
2022-08-29 22:36:35 -0700

    add proper error when endpoints lack coverage

---
+++ src/exceptions/missing-endpoint-coverage.ts
@@ -0,0 +1,21 @@
+export default class MissingEndpointCoverage extends Error {
+  private endpointKeys: string[]
+
+  constructor(endpointKeys: string[]) {
+    super()
+    this.endpointKeys = endpointKeys
+  }
+
+  get message() {
+    return `
+      ATTENTION:
+        The following endpoints have not been checked by your suite for compliance:
+
+        ${this.endpointKeys.join(",\n")}
+
+      In order to obtain full coverage, make sure you run the .toPassCompliance on all
+      endpoints within your api-contract.json file.
+    `
+  }
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import './extensions/jest'
 import endpointTracker from './helpers/endpoint-tracker'
 import readApiContractJSON from './helpers/readApiContractJSON'
+import MissingEndpointCoverage from './exceptions/missing-endpoint-coverage'
 
 export function expectCompleteCompliance() {
   const endpoints = readApiContractJSON()
   const unprocessedEndpoints = Object
     .keys(endpoints)
     .filter(endpointKey => endpointKey !== 'config' && !endpointTracker.endpointsProcessed[endpointKey])
-  console.log("HIIIII", unprocessedEndpoints, endpointTracker.endpointsProcessed)
+
+  if (!!unprocessedEndpoints.length) throw new MissingEndpointCoverage(unprocessedEndpoints)
 }
