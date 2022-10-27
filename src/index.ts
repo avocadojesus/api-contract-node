ec906f8d3af1b773b06481a6552b5f1e9df74fac
avocadojesus <fred@platejoy.com>
2022-10-27 16:36:43 -0700

    use correct endpoint

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import _resetEndpointMocks from './server/helpers/resetEndpointMocks'
 export const expectFullCompliance = _expectFullCompliance
 export const mockEndpoint = async (httpMethod: string, path: string, payload: {[key: string]: any}) => {
   const res = await superagent
-    .post('http://localhost:4000/__api_contract_internal/create-mock?httpMethod')
+    .post('http://localhost:4000/__api_contract_internal/mock_endpoint?httpMethod')
     .send({ httpMethod, path, payload })
   console.log(res)
   // _mockEndpoint(httpMethod, path, payload)
