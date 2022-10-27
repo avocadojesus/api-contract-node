d5390f4aef343d035eba57351e0e2f3849b97ea4
avocadojesus <fred@platejoy.com>
2022-10-27 16:40:53 -0700

    parse json

--- src/server/helpers/createServer.ts
+++ src/server/helpers/createServer.ts
@@ -4,5 +4,8 @@ import createRoutes from './createRoutes'
 export default function createServer(endpointJSONPath?: string) {
   const app: Express = express()
   createRoutes(app, endpointJSONPath)
+
+  app.use(express.json())
+
   return app
 }
