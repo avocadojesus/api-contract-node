30f1dee274ca65008b6a6b6dd550b8235bcac5ba
avocadojesus <fred@platejoy.com>
2022-10-28 08:09:02 -0700

    remove debug

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,11 @@ import _resetEndpointMocks from './server/helpers/resetEndpointMocks'
 
 export const expectFullCompliance = _expectFullCompliance
 export const mockEndpoint = async (httpMethod: string, path: string, payload: {[key: string]: any}) => {
-  console.log('SENDING', { httpMethod, path, payload })
   try {
     const res = await superagent
       .post('http://localhost:4000/__api_contract_internal/mock_endpoint')
       .send({ httpMethod, path, payload })
       .set('Accept', 'application/json')
-    console.log(res)
   } catch (error) {
     throw 'FAILED TO MOCK ENDPOINT'
   }
