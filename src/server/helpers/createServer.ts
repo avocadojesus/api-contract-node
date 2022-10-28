b8d9c9ac0f9e2fef00d00822b18503685a560b6b
avocadojesus <fred@platejoy.com>
2022-10-27 17:36:28 -0700

    do json before routes

--- src/server/helpers/createServer.ts
+++ src/server/helpers/createServer.ts
@@ -4,11 +4,11 @@ import createRoutes from './createRoutes'
 
 export default function createServer(endpointJSONPath?: string) {
   const app: Express = express()
-  createRoutes(app, endpointJSONPath)
+  app.use(express.json())
 
+  createRoutes(app, endpointJSONPath)
   // app.use(bodyParser.json())
   // app.use(bodyParser.urlencoded({ extended: true }))
-  app.use(express.json())
   // app.use(express.urlencoded({ extended: true }))
   // app.use(express.urlencoded({ extended: true}))
 
