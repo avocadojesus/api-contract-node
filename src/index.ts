4b7e09e89b62d5b49338aa75d3c69fabd62323d1
avocadojesus <fred@platejoy.com>
2022-10-27 17:25:05 -0700

    try superagent

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // importing this file will automatically extend jest
 import './extensions/jest'
-import axios from 'axios'
+import superagent from 'superagent'
 
 import _expectFullCompliance from './extensions/jest/expectFullCompliance'
 import _mockEndpoint from './server/helpers/mockEndpoint'
@@ -9,11 +9,10 @@ import _resetEndpointMocks from './server/helpers/resetEndpointMocks'
 export const expectFullCompliance = _expectFullCompliance
 export const mockEndpoint = async (httpMethod: string, path: string, payload: {[key: string]: any}) => {
   console.log('SENDING', { httpMethod, path, payload })
-  const res = await axios
-    .post(
-      'http://localhost:4000/__api_contract_internal/mock_endpoint',
-      { httpMethod, path, payload }
-    )
+  const res = await superagent
+    .post('http://localhost:4000/__api_contract_internal/mock_endpoint')
+    .send({ httpMethod, path, payload })
+    .set('Accept', 'application/json')
   console.log(res)
   // _mockEndpoint(httpMethod, path, payload)
 }
