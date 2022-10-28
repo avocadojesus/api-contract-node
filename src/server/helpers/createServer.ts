52bbb76d97f35faeda8099e4af9dbc1c69813553
avocadojesus <fred@platejoy.com>
2022-10-28 08:50:09 -0700

    add cors

--- src/server/helpers/createServer.ts
+++ src/server/helpers/createServer.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express'
 import createRoutes from './createRoutes'
+import cors from 'cors'
 
 export default function createServer(endpointJSONPath?: string) {
   const app: Express = express()
 
   app.use(express.json())
+  app.use(cors())
   createRoutes(app, endpointJSONPath)
 
   return app
