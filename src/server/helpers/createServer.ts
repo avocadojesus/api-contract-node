465c99efe8acb021f8c462a2c647d2fc0a231952
avocadojesus <fred@platejoy.com>
2022-10-27 17:04:08 -0700

    try using body-parser

--- src/server/helpers/createServer.ts
+++ src/server/helpers/createServer.ts
@@ -1,11 +1,12 @@
 import express, { Express } from 'express'
+import bodyParser from 'body-parser'
 import createRoutes from './createRoutes'
 
 export default function createServer(endpointJSONPath?: string) {
   const app: Express = express()
   createRoutes(app, endpointJSONPath)
 
-  app.use(express.json())
+  app.use(bodyParser.json())
   // app.use(express.urlencoded({ extended: true}))
 
   return app
